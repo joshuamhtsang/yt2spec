596e81274d1944bf61836b8a2369d2fbed11276c
Man Hong Tsang <joshua.mh.tsang@gmail.com>
2019-01-08 19:14:17 +0000

    Moved wav file extraction to module too.

--- audio_ninja.py
+++ audio_ninja.py
@@ -1,6 +1,15 @@
 import subprocess
 
 
+def extract_wav(filename, outname):
+    cmd = "ffmpeg -i %s -vn %s" % (filename, outname)
+    p = subprocess.Popen(cmd.split(" "),
+                         stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE)
+    (output, stderr) = p.communicate()
+    print(output, stderr)
+
+
 # Cut out a section of audio.
 def audio_cut(filename, t_start, cut_length, outname):
     cmd = "ffmpeg -ss %s -t %s -i %s %s" % (
--- video2spectrogram.py
+++ video2spectrogram.py
@@ -7,9 +7,6 @@ import matplotlib.pyplot as plt
 import audio_ninja
 
 
-
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--vid_name", help="Video file to process.",
@@ -23,13 +20,9 @@ if __name__ == '__main__':
     print("Input video name: " + args.vid_name)
 
     # Extract audio *.wav from the video file.
-    cmd = "ffmpeg -i %s -vn %s" % (args.vid_name, args.out_name)
-    p = subprocess.Popen(cmd.split(" "),
-                         stdin=subprocess.PIPE, stdout=subprocess.PIPE,
-                         stderr=subprocess.PIPE)
-    (output, stderr) = p.communicate()
-    print(output, stderr)
+    audio_ninja.extract_wav(args.vid_name, args.out_name)
 
+    # Cut out a section of audio.
     audio_ninja.audio_cut(args.out_name, args.t_start, args.cut_length, args.out_name.split(".")[0] + "_100.wav")
 
     # Generate spectrogram using librosa.
