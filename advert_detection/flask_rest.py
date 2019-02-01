e4e067c0a0241b0a9d2b2ad03b22b5e3c211674e
Man Hong Tsang <joshua.mh.tsang@gmail.com>
2019-02-01 18:43:07 +0000

    Yay!  I can return an image url now based on static folder.

--- advert_detection/flask_rest.py
+++ advert_detection/flask_rest.py
@@ -1,8 +1,10 @@
 from flask_restful import Api
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, url_for
 from flask_cors import CORS
 
 from youtube_download import downloader
+from audio_ninja import extract_wav, audio_cut
+from video2spectrogram import audio2spectrogram
 
 app = Flask(__name__)
 CORS(app)
@@ -36,13 +38,37 @@ def yt2melspec():
 def testjson():
     req_data = request.get_json()
 
-    url = req_data["url"]
+    yt_url = req_data["url"]
+
+    video_filename = downloader(yt_url)
+    print("Video %s has been downloaded!" % video_filename)
+
+    audio_filename = extract_wav(video_filename, video_filename.split(".")[0] + '.wav')
+    # Restrict to first 60 seconds for now.
+    audio_cut_filename = audio_cut(audio_filename, 1, 61, audio_filename.split(".")[0])
+    spec_filename = audio2spectrogram(audio_cut_filename)
+
+    spec_url = 'placeholder'
 
     response = {
-        "url": url
+        "url": yt_url,
+        "spec_url": spec_url
     }
 
     return jsonify(response)
+
+
+@app.route("/returnimg", methods=['GET'])
+def return_img():
+
+    img_url = url_for('static', filename='curry.png')
+
+    response ={
+        "img_url": img_url
+    }
+
+    #return jsonify(response)
+    return '<img src=' + img_url + '>'
 
 
 if __name__ == "__main__":
